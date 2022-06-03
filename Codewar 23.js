cefa46cf945a8767e4f3102efef5ae61232e163b
Harpaul Padda <102876473+HSPandy@users.noreply.github.com>
2022-06-02 21:42:05 -0700

    Initial commit

---
+++ Codewar 23.js
@@ -0,0 +1,6 @@
+//Write a function that returns a string in which firstname is swapped with last name.
+
+function nameShuffler(str){
+    const fullName = str.split(" ");
+    return [fullName[1], fullName[0]].join(" ");
+  }
